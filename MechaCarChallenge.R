efc0740908c81bc5dffcfc4be4c2d853e6a3cda0
Victoria Morales <vcm41879@yahoo.com>
2021-03-14 09:49:08 -0700

    Updated del 1 file

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,4 +1,4 @@
 library(dplyr)
 MechaCar_data <- read.csv(file="MechaCar_mpg.csv")
-lm(vehicle_length ~ vehicle_weight + spoiler_angle + ground_clearance + AWD + mpg,data=MechaCar_data) #generate multiple line regression model
-summary(lm(vehicle_length ~ vehicle_weight + spoiler_angle + ground_clearance + AWD + mpg,data=MechaCar_data)) #generate summary statistics
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar_data) #generate multiple line regression model
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar_data)) #generate summary statistics
